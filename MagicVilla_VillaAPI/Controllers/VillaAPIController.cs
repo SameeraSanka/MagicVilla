21a86e1831b940998992a44ed5560d7b15593d76
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-07-13 22:30:01 +0530

    2 004 First API Endpoint - HTTPGET

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+	//[Route("api/VillaAPIController")] mehema dannath puluwan
 	[Route("api/[controller]")]
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
+		[HttpGet]
+		public IEnumerable<Villa> GetVillas()
+		{
+			return new List<Villa>
+			{
+				new Villa{Id=1,Name="Pool View"},
+				new Villa{Id=2,Name="Beach View"}
+			};
+		}
 	}
 }
