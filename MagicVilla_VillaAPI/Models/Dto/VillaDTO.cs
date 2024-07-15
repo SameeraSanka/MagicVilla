221cee8452ad710f75de5e25c92da39ab4c6efcb
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-07-15 20:30:28 +0530

    4 004 Entity Framework Core Database Models

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -11,8 +11,19 @@ namespace MagicVilla_VillaAPI.Models.Dto
 		[MaxLength(30)]
 		public string Name { get; set; }
 
-		public int Occupency { get; set; }
+		public string Details { get; set; }
+
+		[Required]
+		public double Rate { get; set; }
 
 		public int Sqft { get; set; }
+
+		public int Occupancy { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public string Amenity { get; set; }
+
+
 	}
 }
